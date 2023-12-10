730438dd5278e4954b1533368023a0a96f2e9b1f
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-10 16:45:28 +0545

    without login

--- items/views.py
+++ items/views.py
@@ -4,7 +4,9 @@ from .models import Item
 # Create your views here.
 def item_detail_view(request,pk):
     item=get_object_or_404(Item,pk=pk)
+    related_item=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
     # print(items.name)
     return render(request,'items/detail.html',{
-        'item':item
+        'item':item,
+        'related_item':related_item
     })
