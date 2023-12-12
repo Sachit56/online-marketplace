772c7afc2ac584d00ca3af7f680c90493dcaf964
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-12 20:38:06 +0545

    delete added

--- dashboard/views.py
+++ dashboard/views.py
@@ -12,8 +12,3 @@ def DashboardView(request):
         'items':items
     })
 
-def DeleteView(request,pk):
-    item=get_object_or_404(Item,pk=pk,created_by=request.user)
-    item.delete()
-
-    return redirect('dashboard:index')
